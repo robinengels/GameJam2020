a180771bf4fd9a540c94fb6fab96067e81155b30
Dorian Gruselin <dorian.gruselin@hotmail.com>
2020-11-06 21:11:46 +0100

    Enemy stop and layer checking

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -8,23 +8,29 @@ namespace IPL
     {
         [SerializeField] private Animator animator;
         [SerializeField] private float speed;
+        [SerializeField] private LayerMask playerLayer;
 
+        private bool _isMoving;
         private Transform _transform;
         private static readonly int _Attack = Animator.StringToHash("Attack");
 
         private void Awake()
         {
             _transform = transform;
+            _isMoving = true;
         }
 
         private void Update()
         {
+            if (!_isMoving) return;
             var movement = Time.deltaTime * speed * Vector3.right;
             _transform.position += movement;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            if (playerLayer == (1 << other.gameObject.layer | playerLayer)) return;
+            _isMoving = false;
             StartCoroutine(PlayAttackAnimation());
             StartCoroutine(GameOver());
         }
