74181f3963da0055c0a61630276e66e18a18376c
Apix-Raser <57746726+Apix-Raser@users.noreply.github.com>
2020-11-06 18:55:53 +0100

    Add files via upload

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask collisionLayer;
+
+
+    private Vector3 velocity = Vector3.zero;
+    private bool IsGrounded;
+
+    private void FixedUpdate()
+    {
+        IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.5f, collisionLayer);
+
+    }
+
+    void Update()
+    {
+        StartCoroutine(IncreaseSpeed());
+        var velocity = Time.deltaTime * speed * Vector3.right;
+        transform.position += velocity;
+
+        //rb.AddForce(Vector3.right * Time.deltaTime * 0.01f);
+
+        //rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector3.right, ref velocity, .05f);
+
+        //rb.AddForce(Vector3.right * speed);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+        }
+
+    }
+
+    private IEnumerator IncreaseSpeed()
+    {
+        yield return new WaitForSeconds(1f);
+        speed += (float)0.00001;
+
+    }
+}
