a2a357e0bcc26c8f0efd32ec093d712aa78cb8fc
Dorian Gruselin <dorian.gruselin@hotmail.com>
2020-11-07 13:50:25 +0100

    Front zombies with pooling system

---
+++ Assets/Scripts/Pooling/ObjectPool.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Pooling
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] private GameObject pooledObject;
+        [SerializeField] private int maxActiveInstances = 2;
+        
+        private Queue<GameObject> _pool = new Queue<GameObject>(5);
+        private static Dictionary<GameObject, ObjectPool> _pools = new Dictionary<GameObject, ObjectPool>(5);
+        private Transform _transform;
+
+        public static ObjectPool GetPool(GameObject pooledObject)
+        {
+            if (_pools.TryGetValue(pooledObject, out var pool) == false)
+            {
+                pool = new GameObject(pooledObject.name  + " Pool", typeof(ObjectPool)).GetComponent<ObjectPool>();
+                pool.pooledObject = pooledObject;
+                _pools[pooledObject] = pool;
+            }
+            return pool;
+        }
+
+        private void Awake()
+        {
+            _transform = transform;
+            if (pooledObject)
+                _pools[pooledObject] = this;
+        }
+
+        public bool TryAcquire(out GameObject toAcquire)
+        {
+            if (_pool.Any())
+            {
+                toAcquire = _pool.Dequeue();
+                toAcquire.SetActive(true);
+                return true;
+            }
+            toAcquire = Instantiate(pooledObject, _transform);
+            var pooled = toAcquire.GetComponent<Pooled>();
+            if (!pooled)
+                pooled = toAcquire.AddComponent<Pooled>();
+            pooled.pool = this;
+            return true;
+        }
+
+        public bool TryRelease(GameObject toRelease)
+        {
+            if (toRelease.GetComponent<Pooled>()?.pool != this)
+                return false;
+            toRelease.SetActive(false);
+            StartCoroutine(Enqueue(toRelease));
+            return true;
+        }
+
+        private IEnumerator Enqueue(GameObject obj)
+        {
+            yield return null;
+            _pool.Enqueue(obj);
+        }
+    }
+}
---
+++ Assets/Scripts/Pooling/PoolableEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Pooling
+{
+    public class PoolableEnemy : MonoBehaviour
+    {
+        private Camera _mainCamera;
+        private Transform _mainCameraTransform;
+        private Transform _transform;
+        
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _mainCameraTransform = _mainCamera.transform;
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            var xReleaseThreshold = _mainCameraTransform.position.x - (_mainCamera.orthographicSize * 3f);
+            if (_transform.position.x <= xReleaseThreshold)
+                gameObject.TryRelease();
+        }
+    }
+}
