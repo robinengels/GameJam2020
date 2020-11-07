b934e2a57989c8b280949af707d9556d936edc4e
Robin <robin.engels@gmail.com>
2020-11-08 00:37:24 +0100

    MainMenuFixed

---
+++ Assets/MenuButtonInitialiser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonInitialiser : MonoBehaviour
+{
+
+    [SerializeField] private Button play;
+    [SerializeField] private Button quitToMenu;
+
+    private void Awake()
+    {
+        play.onClick.AddListener(GameManager.Instance.NewGame);
+        quitToMenu.onClick.AddListener(GameManager.Instance.MainMenu);
+    }
+}
